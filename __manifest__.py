a44a1e8ed57f250634565f9edc1e1f924062a11f
Dabhi Harpal <hdabhi902@rku.ac.in>
2024-11-20 19:46:56 +0530

    [update]- manifest file content

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'TMS',
+    'version': '0.1',
+    'category': 'Sales/Sales',
+    'summary': 'Task Management System',
+    'description': """
+This module contains all about task management system and its features.
+    """,
+    'depends': [
+        'base',
+    ],
+    'data': [
+
+    ],
+
+    'installable': True,
+    'license': 'LGPL-3',
+}
